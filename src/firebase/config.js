45ee8b8e9caaba01030fa97af63322334ed86a79
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-10-26 20:00:29 -0400

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBu5TyR4apysuxa1cGcKJ8GHRqZJHDZX2A",
+  authDomain: "hunt-af68a.firebaseapp.com",
+  databaseURL: "https://hunt-af68a.firebaseio.com",
+  projectId: "hunt-af68a",
+  storageBucket: "hunt-af68a.appspot.com",
+  messagingSenderId: "300544913921",
+  appId: "1:300544913921:web:bb3b39a96a3c48056f0600",
+};
+
+export default firebaseConfig;
