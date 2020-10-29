6cd0f075c02d112803e4e83c854099875e33c001
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-10-29 13:53:46 -0400

    Login Page and Validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,29 +11,78 @@ import {
 } from "@ionic/react";
 import React from "react";
 import NavHeader from "../components/Header/NavHeader";
+import { toast } from "../utils/toast";
+import useFormValidation from "../hooks/useFormValidation";
+import validateLogin from "../components/auth/validateLogin";
+import firebase from "../firebase";
 
-const Login = () => {
+const INITIAL_STATE = {
+  email: "",
+  password: "",
+};
+
+const Login = (props) => {
+  const {
+    handleSubmit,
+    handleChange,
+    values,
+    isSubmitting,
+  } = useFormValidation(INITIAL_STATE, validateLogin, authenticateUser);
+  const [busy, setBusy] = React.useState(false);
+
+  async function authenticateUser() {
+    setBusy(true);
+    const { email, password } = values;
+    try {
+      await firebase.login(email, password);
+      toast("You have logged in successfully!");
+      props.history.push("/");
+    } catch (err) {
+      console.error("Authentication Error", err);
+      toast(err.message);
+    }
+    setBusy(false);
+  }
+
   return (
     <IonPage>
       <NavHeader title="Log In" />
       <IonContent>
         <IonItem lines="full">
           <IonLabel position="floating">Email</IonLabel>
-          <IonInput name="email" type="text" required></IonInput>
+          <IonInput
+            name="email"
+            value={values.email}
+            type="text"
+            onIonChange={handleChange}
+            required
+          ></IonInput>
         </IonItem>
         <IonItem lines="full">
           <IonLabel position="floating">Password</IonLabel>
-          <IonInput name="password" type="password" required></IonInput>
+          <IonInput
+            name="password"
+            value={values.password}
+            type="password"
+            onIonChange={handleChange}
+            required
+          ></IonInput>
         </IonItem>
         <IonRow>
           <IonCol>
-            <IonButton type="submit" color="primary" expand="block">
+            <IonButton
+              type="submit"
+              color="primary"
+              expand="block"
+              onClick={handleSubmit}
+              disabled={isSubmitting}
+            >
               Log In
             </IonButton>
           </IonCol>
         </IonRow>
         <IonRow>
-          <IonCol>
+          <IonCol class="ion-text-center ion-padding vertical">
             <IonRouterLink routerLink={"/forgot"}>
               Forgot Password?
             </IonRouterLink>
