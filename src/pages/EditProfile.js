d45d8502b21931d4cfb200d2a2ff6b55047317c3
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-11-11 16:46:54 -0500

    Edit Profile Page and Validation

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -7,34 +7,136 @@ import {
   IonPage,
   IonRow,
   IonCol,
+  IonLoading,
 } from "@ionic/react";
 import React from "react";
 import NavHeader from "../components/Header/NavHeader";
+import { toast } from "../utils/toast";
+import useFormValidation from "../hooks/useFormValidation";
+import validateEditProfile from "../components/auth/validateEditProfile";
+import firebase from "../firebase";
+import UserContext from "../contexts/UserContext";
 
-const EditProfile = () => {
+const EditProfile = (props) => {
+  const { user, setUser } = React.useContext(UserContext);
+
+  const INITIAL_STATE = {
+    name: user && user.displayName,
+    email: user && user.email,
+    newPassword: "",
+    currentPassword: "",
+  };
+
+  const {
+    handleSubmit,
+    handleChange,
+    setValues,
+    values,
+    isSubmitting,
+  } = useFormValidation(INITIAL_STATE, validateEditProfile, authenticateUser);
+
+  const [busy, setBusy] = React.useState(false);
+
+  async function reauthenticate(email, password) {
+    const credential = firebase.app.auth.EmailAuthProvider.credential(
+      email,
+      password
+    );
+    try {
+      await user.reauthenticateWithCredential(credential);
+      console.log("Reauthentication successful");
+    } catch (err) {
+      console.error("Profile Update Error", err);
+      toast(err.message);
+    }
+  }
+
+  async function updateProfileItems(name, email, password) {
+    await user.updateProfile({ displayName: name });
+    await user.updateEmail(email);
+    if (password) {
+      await user.updatePassword(password);
+    }
+  }
+
+  async function authenticateUser() {
+    setBusy(true);
+    const { name, email, currentPassword, newPassword } = values;
+    try {
+      await reauthenticate(user.email, currentPassword);
+      await updateProfileItems(name, email, newPassword);
+      const result = await firebase.login(
+        email,
+        newPassword || currentPassword
+      );
+      setValues({
+        name: user && user.displayName,
+        email: email && user.email,
+        newPassword: "",
+        currentPassword: "",
+      });
+      setUser(result.user);
+      toast("You have updated your profile successfully.");
+      props.history.push("/profile");
+    } catch (err) {
+      console.error("Profile Update Error", err);
+      toast(err.message);
+    }
+    setBusy(false);
+  }
+
   return (
     <IonPage>
       <NavHeader title="Log In" />
+      <IonLoading message={"Please wait..."} isOpen={busy} />
       <IonContent>
         <IonItem lines="full">
           <IonLabel position="floating">Username</IonLabel>
-          <IonInput name="name" type="text" required></IonInput>
+          <IonInput
+            name="name"
+            type="text"
+            value={values.name}
+            onIonChange={handleChange}
+            required
+          ></IonInput>
         </IonItem>
         <IonItem lines="full">
           <IonLabel position="floating">Email</IonLabel>
-          <IonInput name="email" type="text" required></IonInput>
+          <IonInput
+            name="email"
+            type="text"
+            value={values.email}
+            onIonChange={handleChange}
+            required
+          ></IonInput>
         </IonItem>
         <IonItem lines="full">
           <IonLabel position="floating">New Password</IonLabel>
-          <IonInput name="newPassword" type="password" required></IonInput>
+          <IonInput
+            name="newPassword"
+            type="password"
+            value={values.newPassword}
+            onIonChange={handleChange}
+          ></IonInput>
         </IonItem>
         <IonItem lines="full">
           <IonLabel position="floating">Current Password</IonLabel>
-          <IonInput name="currentPassword" type="password" required></IonInput>
+          <IonInput
+            name="currentPassword"
+            type="password"
+            value={values.currentPassword}
+            onIonChange={handleChange}
+          ></IonInput>
         </IonItem>
         <IonRow>
           <IonCol>
-            <IonButton type="submit" color="primary" expand="block">
+            <IonButton
+              type="submit"
+              color="primary"
+              expand="block"
+              onClick={handleSubmit}
+              disabled={isSubmitting}
+            >
               Save
             </IonButton>
           </IonCol>
