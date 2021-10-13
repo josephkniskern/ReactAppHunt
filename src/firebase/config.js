605b397875e666bab19ccc0cf8b691461f01514b
josephkniskern <48452437+josephkniskern@users.noreply.github.com>
2021-10-13 14:41:27 -0400

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-  apiKey: ###,
-  authDomain: "hunt-af68a.firebaseapp.com",
-  databaseURL: "https://hunt-af68a.firebaseio.com",
-  projectId: "hunt-af68a",
-  storageBucket: "hunt-af68a.appspot.com",
-  messagingSenderId: "300544913921",
-  appId: "1:300544913921:web:bb3b39a96a3c48056f0600",
+
 };
 
 export default firebaseConfig;
